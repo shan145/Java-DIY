8bb1b1064c78b0da8df90e11e31950c8918f4998
Samuel Han <sh779@cornell.edu>
2020-12-13 11:19:54 -0500

    First commit

---
+++ src/sorting/Sorters.java
@@ -0,0 +1,60 @@
+package sorting;
+
+public class Sorters {
+	
+	/*
+	 * Selection Sorts (iterative and recursive)
+	 */
+	public static <T extends Comparable<? super T>> void selectionSort(T[] array, int start, int end) {
+		for(int index = start; index <= end; index++) {
+			int indexOfSmallest = getIndexOfNextSmallest(array, index, end);
+			swap(array, index, indexOfSmallest);
+		}
+	}
+	
+	public static <T extends Comparable<? super T>> void recursiveSelectionSort(T[] array, int start, int end) {
+		if (start < end) {
+			int indexOfSmallest = getIndexOfNextSmallest(array, start, end);
+			swap(array, start, indexOfSmallest);
+			recursiveSelectionSort(array, start+1, end);
+		}
+	}
+	
+	public static <T extends Comparable<? super T>> int getIndexOfNextSmallest(T[] array, int index, int end) {
+		T min = array[index];
+		int indexOfMin = index;
+		for(int i = index+1; i <= end; i++) {
+			if(array[i].compareTo(min) < 0) {
+				min = array[i];
+				indexOfMin = i;
+			}
+		}
+		return indexOfMin;
+	}
+	
+	public static <T> void swap (T[] array, int index, int indexOfSmallest) {
+		T temp = array[index];
+		array[index] = array[indexOfSmallest];
+		array[indexOfSmallest] = temp;
+	}
+	
+	
+	/*
+	 * Insertion Sorts (iterative and recursive)
+	 */
+	public static <T extends Comparable<? super T>> void insertionSort(T[] array, int start, int end) {
+		for(int index = start+1; index <= end; index++) {
+			T nextToInsert = array[index];
+			insertInOrder(nextToInsert, array, start, index);
+		}
+	}
+	
+	public static <T extends Comparable<? super T>> void insertInOrder(T nextToInsert, T[] array, int start, int currentIndex) {
+		while((currentIndex > start) && ((nextToInsert.compareTo(array[currentIndex-1])) < 0)) {
+			array[currentIndex] = array[currentIndex-1];
+			currentIndex--;
+		}
+		array[currentIndex] = nextToInsert;
+	}
+	
+}
