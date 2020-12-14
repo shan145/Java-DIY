5983b1f82120e77ed80162d616081276b804c872
Samuel Han <sh779@cornell.edu>
2020-12-13 20:47:43 -0500

    Added recursive mergesort and unit tests

--- src/sorting/Sorters.java
+++ src/sorting/Sorters.java
@@ -77,4 +77,60 @@ public class Sorters {
 		}
 	}
 	
+	/*
+	 * Merge Sorts (iterative and recursive)
+	 */
+	public static <T extends Comparable<? super T>> void recursiveMergeSort(T[] array, int start, int end) {
+		@SuppressWarnings("unchecked")
+		T[] tempArray = (T[])new Comparable<?>[array.length];
+		mergeSort(array, tempArray, start, end);
+	}
+	
+	private static <T extends Comparable<? super T>> void mergeSort(T[] array, T[] tempArray, int start, int end) {
+		if (start < end) {
+			int mid = (start+end)/2;
+			mergeSort(array, tempArray, start, mid);
+			mergeSort(array, tempArray, mid+1, end);
+			merge(array, tempArray, start, mid, end);
+		}
+	}
+	
+	private static <T extends Comparable<? super T>> void merge(T[] array, T[] tempArray, int start, int mid, int end) {
+		int startHalf1 = start;
+		int endHalf1 = mid;
+		int startHalf2 = mid+1;
+		int endHalf2 = end;
+		
+		int index = start; //index of tempArray where space is available
+		
+		while((startHalf1 <= endHalf1) && (startHalf2 <= endHalf2)) {
+			if(array[startHalf1].compareTo(array[startHalf2]) <= 0) {
+				tempArray[index] = array[startHalf1];
+				startHalf1++;
+			}
+			else {
+				tempArray[index] = array[startHalf2];
+				startHalf2++;
+			}
+			index++;
+		}
+		
+		// Copy over remaining from other subarray to tempArray
+		while(startHalf1 <= endHalf1) {
+			tempArray[index] = array[startHalf1];
+			startHalf1++;
+			index++;
+		}
+		while(startHalf2 <= endHalf2) {
+			tempArray[index] = array[startHalf2];
+			startHalf2++;
+			index++;
+		}
+		
+		// Copy from tempArray to regular array
+		for(int k = start; k <= end; k++) {
+			array[k] = tempArray[k];
+		}
+	}
+	
 }
--- src/sorting/SortersTest.java
+++ src/sorting/SortersTest.java
@@ -60,5 +60,18 @@ class SortersTest {
 		Sorters.recursiveInsertionSort(array2, 1, array2.length-2);
 		assertTrue(Arrays.equals(expectedArray2, array2));
 	}
+	
+	@Test
+	void testRecursiveMergeSort() {
+		Integer[] array = new Integer[] {9, 1, 3, 4, 2, 8, 6, 5, 7};
+		Integer[] expectedArray = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		Sorters.recursiveMergeSort(array, 0, array.length-1);
+		assertTrue(Arrays.equals(expectedArray, array));
+
+		Integer[] array2 = new Integer[] {9, 5, 4, 2, 6};
+		Integer[] expectedArray2 = new Integer[] {9, 2, 4, 5, 6};
+		Sorters.recursiveMergeSort(array2, 1, array2.length-2);
+		assertTrue(Arrays.equals(expectedArray2, array2));
+	}
 
 }
