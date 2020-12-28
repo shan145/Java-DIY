7e292549cdd53fbc75884b1dbd8472b3717fae9e
Samuel Han <sh779@cornell.edu>
2020-12-28 16:56:15 -0500

    Added bag interface

---
+++ src/bag/BagInterface.java
@@ -0,0 +1,21 @@
+package bag;
+
+public interface BagInterface<T> {
+	public int getCurrentSize();
+	
+	public boolean isEmpty();
+	
+	public boolean add(T newEntry);
+	
+	public T remove();
+	
+	public boolean remove (T anEntry);
+	
+	public void clear();
+	
+	public int getFrequencyOf(T anEntry);
+	
+	public boolean contains(T anEntry);
+	
+	public T[] toArray();
+}
